801e2a16f7f2b5a5a098ed3bb8e2c01a67091ad0
mdemartino26 <m.demartino99@gmail.com>
2025-03-17 23:46:14 -0300

    commit inicial, vista no disponible en computadoras

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+     <p className="celular">Este contenido esta disponible solamente en dispositivos celulares.</p>
+    </div>
+  );
+}
+
+export default App;
