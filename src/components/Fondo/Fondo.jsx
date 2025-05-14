2169f8b903ce12832c1528c3087e0b13d7f994ff
mdemartino26 <m.demartino99@gmail.com>
2025-05-14 18:11:41 -0300

    intento hacer el fondo, no pude

---
+++ src/components/Fondo/Fondo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './styles.css'
+
+export default function Fondo() {
+  return (
+    <div className='fondo'>
+       <img src="./img/logo.png" alt="logo"/>
+    </div>
+  )
+}
--- src/pages/Expediente/Expediente.js
+++ src/pages/Expediente/Expediente.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Input from '../../components/Input/Input'
+import Fondo from '../../components/Fondo/Fondo'
 
 export default function Expediente() {
   return (
     <div>
+      <Fondo/>
         <h2>Expediente</h2>
         <Input redirectPath="/menu" />
     </div>
